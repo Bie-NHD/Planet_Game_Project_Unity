296986653fa2240a749272a1dc84dead49904448
Duyen Nguyen <duyennguyen.modobom@gmail.com>
2025-04-11 16:20:04 +0700

    hot-fix: game over not triggerred

--- Assets/Scripts/GameOverCollider.cs
+++ Assets/Scripts/GameOverCollider.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class GameOverCollider : MonoBehaviour
 {
-
     private bool m_isColliding = false;
 
     private float m_timer = 0f;
@@ -17,7 +16,6 @@ public class GameOverCollider : MonoBehaviour
 
     void OnTriggerStay2D(Collider2D collision)
     {
-
         if (m_isColliding)
         {
             m_timer += Time.deltaTime;
@@ -32,15 +30,19 @@ public class GameOverCollider : MonoBehaviour
                 m_timer = 0f; // reset the timer
             }
         }
-
-
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        // So far, not sure how to check there is NO collision with all objects
-        m_isColliding = false;
-        m_timer = 0f;
+        Rigidbody2D rb = other.attachedRigidbody;
+        if (rb != null)
+        {
+            if (rb.linearVelocityY < 0)
+            {
+                m_isColliding = false; // reset the collision state
+                m_timer = 0f; // reset the timer
+            }
+        }
     }
 
     bool HasRigidBody2D(Collider2D collider2D)
